cf6bb6dc0a93737c888aafd0a7a7011ea20838eb
Matheus Rocha <49811477+mthsrocha@users.noreply.github.com>
2021-11-16 08:32:32 -0300

    GO database att

--- internal/database/database.go
+++ internal/database/database.go
@@ -4,10 +4,13 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+
+	_ "github.com/lib/pq"
 )
 
 type Database struct {
 	Host    string
+	Port 	int
 	dbname  string
 	user    string
 	passw   string
@@ -17,15 +20,16 @@ type Database struct {
 func Connect() *sql.DB {
 
 	db := Database{
-		Host: "127.0.0.1",
-		dbname: "izanami",
-		user: "root",
-		passw: "root",
+		Host:    "127.0.0.1",
+		dbname:  "izanami",
+		user:    "root",
+		passw:   "root",
 		disable: "disable",
 	}
 
 	connection := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=%s", db.user, db.dbname, db.passw,
 		db.Host, db.disable)
+	fmt.Println(connection)
 	database, err := sql.Open("postgres", connection)
 	if err != nil {
 		log.Println("Error: ", err)
